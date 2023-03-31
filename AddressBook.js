87a931d22752a533649fa2c633bbbd2cf93c89d2
subzero <shubham8111994@gmail.com>
2023-03-31 17:14:16 +0530

    added feature to update details using name

--- AddressBook.js
+++ AddressBook.js
@@ -66,6 +66,27 @@ class AddressBookContact {
       this.state = state;
     } else throw 'address is incorrect please enter 4 digit';
   }
+  getZip() {
+    return this.zip;
+  }
+  setZip(zip) {
+    this.zip = zip;
+  }
+  getPhone() {
+    return this.phone;
+  }
+
+  setPhone(phone) {
+    this.phone = phone;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  setEmail(email) {
+    this.email = email;
+  }
   toString() {
     return (
       'FirstName:' +
@@ -88,7 +109,7 @@ class AddressBookContact {
   }
 }
 
-let contacts = new AddressBookContact(
+let shubham = new AddressBookContact(
   'shubham',
   'singh',
   'karnataka',
@@ -99,7 +120,7 @@ let contacts = new AddressBookContact(
   'shubh@am'
 );
 let addressBookArray = new Array();
-addressBookArray.push(contacts);
+addressBookArray.push(shubham);
 addressBookArray.push(
   new AddressBookContact(
     'kundan',
@@ -112,4 +133,20 @@ addressBookArray.push(
     'kun@dan'
   )
 );
-addressBookArray.forEach((a) => console.log(a.toString()));
+//modify existing contact person on basis of name
+let obj = addressBookArray.find((a) => a.firstName === 'thakur');
+if (obj != undefined) {
+  addressBookArray.pop();
+  obj.setFirstName('Tanuja');
+  obj.setLastName('Chaugule');
+  obj.setAddress('maharashtra');
+  obj.setCity('mumbai');
+  obj.setState('maharashtra');
+  obj.setZip(12345);
+  obj.setPhone(9257268112);
+  obj.setEmail('tanu@ja');
+  addressBookArray.push(obj);
+  addressBookArray.forEach((a) => console.log(a.toString()));
+} else {
+  console.log('Not found');
+}
