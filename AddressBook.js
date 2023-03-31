e75217915bf23c275e4310e048144b5c9dcffa58
subzero <shubham8111994@gmail.com>
2023-03-31 12:45:03 +0530

    added class address book contact

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,21 @@
-console.log('Welcome to address book');
+class AddressBookContact {
+  firstName;
+  lastName;
+  address;
+  city;
+  state;
+  zip;
+  phone;
+  email;
+
+  constructor(...params) {
+    this.firstName = params[0];
+    this.lastName = params[1];
+    this.address = params[2];
+    this.city = params[3];
+    this.state = params[4];
+    this.zip = params[5];
+    this.phone = params[6];
+    this.email = params[7];
+  }
+}
